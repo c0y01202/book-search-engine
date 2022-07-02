9dc0e34e859a64d238710d5077ea6400f908efa4
Claudia Chittim <cchittim10101@gmail.com>
2022-07-02 17:39:02 -0500

    changed auth.js code

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -10,9 +10,8 @@ class AuthService {
 
   // check if user's logged in
   loggedIn() {
-    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    return !!token && !this.isTokenExpired(token); 
   }
 
   // check if token is expired
